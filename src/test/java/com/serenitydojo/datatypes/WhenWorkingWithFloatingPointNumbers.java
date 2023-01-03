b3431a9892eb4cca1f57b949c4a9c8a5d839f42e
Ben Duong <ben.duong@accesshq.com>
2023-01-04 08:55:21 +1100

    Completed Exercises for Data Types

--- src/test/java/com/serenitydojo/datatypes/WhenWorkingWithFloatingPointNumbers.java
+++ src/test/java/com/serenitydojo/datatypes/WhenWorkingWithFloatingPointNumbers.java
@@ -17,7 +17,7 @@ public class WhenWorkingWithFloatingPointNumbers {
         double farenheit = 0.0d;
 
         // TODO: Use a floating point calculation to calculate the farenheit equivalent of the celcius value.
-
+        farenheit = (9.0 / 5.0) * celcius + 32;
         assertThat(farenheit, equalTo(80.6));
     }
 
@@ -26,12 +26,14 @@ public class WhenWorkingWithFloatingPointNumbers {
     // There are 2.20462 pounds in a kilogram
     //
     @Test
-    public void convertMetersToFeet() {
+    public void convertKilogramsToPounds() {
         int weightInKilograms = 50;
         double weightInPounds = 0;
 
         // TODO: Use a floating point calculation to calculate the correct weight in pounds
-
+        double conversionRate = 2.20462;
+        weightInPounds = weightInKilograms * conversionRate;
+        weightInPounds = Math.round(weightInPounds * 1000.0) / 1000.0;
         assertThat(weightInPounds, equalTo(110.231));
 
     }
